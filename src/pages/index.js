1f602e2c6ee20c03ba855a857482a8a405da3b33
Cherry Liang <cherry@centrality.ai>
2019-03-21 16:02:38 +1300

    rename styled component in Homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Header from 'pages/Header';
 import Footer from 'pages/Footer';
 import Download from 'pages/Download';
 
-const HomeContainer = styled.div`
+const HomePage = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -17,7 +17,7 @@ const HomeContainer = styled.div`
 `;
 
 // padding attrs in ContentContainer is temporay layout
-const ContentConatiner = styled.div`
+const Content = styled.div`
   padding: 0 15rem;
   background: ${theme.pageGradient};
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.24);
@@ -27,17 +27,17 @@ const ContentConatiner = styled.div`
 
 const Home = props => {
   return (
-    <HomeContainer>
-      <ContentConatiner>
+    <HomePage>
+      <Content>
         <SEO title="Home" />
         <Helmet>
           <meta name="description" content="Cennz Node landing page" />
         </Helmet>
         <Header />
         <Download props={props} />
-      </ContentConatiner>
+      </Content>
       <Footer />
-    </HomeContainer>
+    </HomePage>
   );
 };
 
